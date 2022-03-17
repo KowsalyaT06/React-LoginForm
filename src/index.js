e4d27d16ea2752eab08a0d5187f9bfd2c1820972
KowsalyaT06 <kowsalyat@dorustree.in>
2022-03-17 18:31:49 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import Parent from './ParentChild/Parent';
 import Register from './Register'
 import './App.css';
